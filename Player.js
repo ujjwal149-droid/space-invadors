8c557a67a878d944ed2ff283420deb421085e176
Ujjawal Tyagi <ujjawaltyagi149@gmail.com>
2026-02-05 20:29:49 +0530

    player movement

--- Player.js
+++ Player.js
@@ -1,7 +1,11 @@
 export default class Player{
-    constructor(canvas) {
+
+    rightPressed = false;
+    leftPressed = false;
+
+    constructor(canvas, velocity) {
         this.canvas = canvas;
-        this.velocity = this.velocity;
+        this.velocity = velocity;
 
         this.x = this.canvas.width/2;
         this.y = this.canvas.height - 75;
@@ -9,9 +13,39 @@ export default class Player{
         this.height = 48;
         this.image = new Image();
         this.image.src = "images/player.png";
+
+        document.addEventListener('keydown', this.keydown);
+        document.addEventListener('keyup', this.keyup);
     }
 
     draw(ctx) {
+        this.move()
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+
+    move() {
+        if(this.rightPressed) {
+            this.x += this.velocity;
+        } else if (this.leftPressed) {
+            this.x += -this.velocity;
+        }
+    }
+
+    keydown = event => {
+        if(event.code == "ArrowRight") {
+            this.rightPressed = true;
+        }
+        if(event.code == "ArrowLeft") {
+            this.leftPressed = true;
+        }
+    }
+
+    keyup = event => {
+        if(event.code == "ArrowRight") {
+            this.rightPressed = false;
+        }
+        if(event.code == "ArrowLeft") {
+            this.leftPressed = false;
+        }
     }
 }
